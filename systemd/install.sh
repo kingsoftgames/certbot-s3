dda960b9044f29898f1feffc2cc31305e968cc7a
Zhiguang Wang <wangzhiguang@kingsoft.com>
2018-07-05 11:01:28 +0800

    chmod 600 /etc/default/certbot-s3

--- systemd/install.sh
+++ systemd/install.sh
@@ -12,6 +12,10 @@ fi
 cp -v certbot-s3.conf /etc/default/certbot-s3
 cp -v certbot-s3.service certbot-s3.timer /etc/systemd/system/
 
+# Only root can access this file,
+# Because it contains sensitive data like AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+chmod 600 /etc/default/certbot-s3
+
 systemctl daemon-reload
 systemctl enable certbot-s3.timer
 systemctl restart certbot-s3.timer
